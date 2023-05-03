86a883704d9e7e697937422c93b86e324d0c4fcf
shravantibn <43784381+shravantibn@users.noreply.github.com>
2023-05-03 15:10:53 +0200

    Update mongoDBRead.py

--- mongoDBRead.py
+++ mongoDBRead.py
@@ -27,8 +27,21 @@ accounts_collection = db.accounts
 document_to_find = {"_id": ObjectId("62d6e0decab6dse1304974ae")}
 
 # Write an expression that retrieves the document matching the query constraint in the ‘accounts’ collection.
-result = accounts_collection. find_one(docunent_to_find)
+result = accounts_collection.find_one(docunent_to_find)
 
 print(result)
+
+###########################################################################################################
+
+##Query to collection based on condition
+documents_to_finds = {"balance":{"$gt":4700}}
+
+cursor = accounts_collection.find(docunent_to_finds)
+num_docs = 0
+for documents in cursor:
+    num_docs += 1
+    pprint.pprint(dcoument)
+    print()
+print("# of documents found:" + str(num_docs))
 
 client.close()
