6c4fc0ccd1ac4946ec73087bb0403ad1e008831b
shravantibn <43784381+shravantibn@users.noreply.github.com>
2023-05-03 14:42:48 +0200

    Update connection.py

--- connection.py
+++ connection.py
@@ -1,1 +1,9 @@
 #Python driver for MongoDB to connect applications.
+from pymongo import MongoClient
+
+MONGODB_URI = "Connection_String_URI"
+
+client = MongoClient(MONGODB_URI)
+
+for db_name in client.list_database_names():
+    print(db_name)
