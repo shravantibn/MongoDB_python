a66f134f41bfdf1b1cf6f9564bac58b74b095393
shravantibn <43784381+shravantibn@users.noreply.github.com>
2023-05-03 15:42:44 +0200

    Update mongoDBDelete.py

--- mongoDBDelete.py
+++ mongoDBDelete.py
@@ -1,1 +1,43 @@
 #Delete the mongoDB collection in python application
+#Delete a Single Document
+# Get reference to 'bank' database
+db = client.bank
+
+# Get a reference to the 'accounts' collection
+accounts_collection = db.accounts
+
+# Filter by ObjectId
+document_to_delete = {"_id": ObjectId("62d6e04ecab6d8e130497485")}
+
+# Search for document before delete
+print("Searching for target document before delete: ")
+pprint.pprint(accounts_collection.find_one(document_to_delete))
+
+# Write an expression that deletes the target account.
+result = accounts_collection.delete_one(document_to_delete)
+
+# Search for document after delete
+print("Searching for target document after delete: ")
+pprint.pprint(accounts_collection.find_one(document_to_delete))
+
+print("Documents deleted: " + str(result.deleted_count))
+
+##########################################################################################################
+#Delete Multiple Documents
+# Filter for accounts with balance less than $2000
+documents_to_delete = {"balance": {"$lt": 2000}}
+
+# Search for sample document before delete
+print("Searching for sample target document before delete: ")
+pprint.pprint(accounts_collection.find_one(documents_to_delete))
+
+# Write an expression that deletes the target accounts.
+result = accounts_collection.delete_many(documents_to_delete)
+
+# Search for sample document after delete
+print("Searching for sample target document after delete: ")
+pprint.pprint(accounts_collection.find_one(documents_to_delete))
+
+print("Documents deleted: " + str(result.deleted_count))
+
+client.close()
