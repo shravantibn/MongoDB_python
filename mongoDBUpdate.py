384a7c9396c1212b8c4cdff7b0ebe5d9f8a83e33
shravantibn <43784381+shravantibn@users.noreply.github.com>
2023-05-03 15:35:40 +0200

    Update mongoDBUpdate.py

--- mongoDBUpdate.py
+++ mongoDBUpdate.py
@@ -1,1 +1,41 @@
 ###Updating the mongoDB collection in python applications
+# Get reference to 'bank' database
+db = client.bank
+
+# Get reference to 'accounts' collection
+accounts_collection = db.accounts
+
+# Filter
+document_to_update = {"_id": ObjectId("62d6e04ecab6d8e130497482")}
+
+# Update
+add_to_balance = {"$inc": {"balance": 100}}
+
+# Print original document
+pprint.pprint(accounts_collection.find_one(document_to_update))
+
+# Write an expression that adds to the target account balance by the specified amount.
+result = accounts_collection.update_one(document_to_update, add_to_balance)
+print("Documents updated: " + str(result.modified_count))
+
+# Print updated document
+pprint.pprint(accounts_collection.find_one(document_to_update))
+
+#########################################################################################
+##Update multiple collection
+# Filter
+select_accounts = {"account_type": "savings"}
+
+# Update
+set_field = {"$set": {"minimum_balance": 100}}
+
+# Write an expression that adds a 'minimum_balance' field to each savings acccount and sets its value to 100.
+result = accounts_collection.update_many(select_accounts, set_field)
+
+print("Documents matched: " + str(result.matched_count))
+print("Documents updated: " + str(result.modified_count))
+pprint.pprint(accounts_collection.find_one(select_accounts))
+
+client.close()
+
+client.close()
